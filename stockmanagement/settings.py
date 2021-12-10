42bc8e0e3941cddf2c82f0e09cfbe104c233fcf6
Eugene Kwaka <eugenekwaka@gmail.com>
2021-12-10 10:15:50 +0300

    added .env file

--- stockmanagement/settings.py
+++ stockmanagement/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'stockmanagement.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  config('DB_NAME'),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
